9ce9f585dcdf38a5de20dec41d69ec35eba5475f
unknown <jobaayoola@gmail.com>
2025-12-26 14:29:16 +0100

    Fixed security: Added .env to gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOVXTP10d-dMt3iv59hEtABpQLgLU0Ijw",
-  authDomain: "todoapp-d999f.firebaseapp.com",
-  projectId: "todoapp-d999f",
-  storageBucket: "todoapp-d999f",
-  messagingSenderId: "24368827782",
-  appId: "1:24368827782:web:9ef631a8af494fa73d7007"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
